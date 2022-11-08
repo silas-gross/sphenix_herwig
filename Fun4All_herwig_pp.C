ddad38c07a7c3889ebc030120f519e0822a6e0e4
Silas K Grossberndt <silas.grossberndt@gmail.com>
2022-11-08 09:15:05 -0500

    first run complete, with standard cuts and hepmc generated and attached

---
+++ Fun4All_herwig_pp.C
@@ -0,0 +1,292 @@
+
+/////This needs a lot of work to understand what all has to be changed to manage it
+//
+#ifndef MACRO_FUN4ALLG4PASS1PP_C
+#define MACRO_FUN4ALLG4PASS1PP_C
+
+#include <GlobalVariables.C>
+
+#include <G4Setup_sPHENIX.C>
+#include <G4_Bbc.C>
+#include <G4_Input.C>
+#include <G4_Production.C>
+
+#include <ffamodules/FlagHandler.h>
+#include <ffamodules/HeadReco.h>
+#include <ffamodules/SyncReco.h>
+#include <ffamodules/XploadInterface.h>
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllSyncManager.h>
+#include <fun4all/Fun4AllUtils.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libffamodules.so)
+
+int Fun4All_G4_Pass1_pp(
+    const int nEvents = 1,
+    const string &outputFile = "G4Hits_herwig_pp_mb-0000000050-00000.root",
+    const string &outdir = ".")
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+
+  //Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
+  PHRandomSeed::Verbosity(1);
+
+  // just if we set some flags somewhere in this macro
+  recoConsts *rc = recoConsts::instance();
+  // By default every random number generator uses
+  // PHRandomSeed() which reads /dev/urandom to get its seed
+  // if the RANDOMSEED flag is set its value is taken as seed
+  // You can either set this to a random value using PHRandomSeed()
+  // which will make all seeds identical (not sure what the point of
+  // this would be:
+  //  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
+  // or set it to a fixed value so you can debug your code
+  //  rc->set_IntFlag("RANDOMSEED", 12345);
+
+// set pp mode for extended readout
+  TRACKING::pp_mode = true;
+
+  //===============
+  // conditions DB flags
+  //===============
+  Enable::XPLOAD = true;
+  // tag
+  rc->set_StringFlag("XPLOAD_TAG",XPLOAD::tag);
+  // database config
+  rc->set_StringFlag("XPLOAD_CONFIG",XPLOAD::config);
+  // 64 bit timestamp
+  rc->set_uint64Flag("TIMESTAMP",XPLOAD::timestamp);
+
+// this extracts the runnumber and segment from the output filename
+// and sets this so the server can pick it up
+  pair<int, int> runseg = Fun4AllUtils::GetRunSegment(outputFile);
+  int runnumber=runseg.first;
+  int segment=runseg.second;
+  if (runnumber != 0)
+  {
+    rc->set_IntFlag("RUNNUMBER",runnumber);
+    Fun4AllSyncManager *syncman = se->getSyncManager();
+    syncman->SegmentNumber(segment);
+  }
+
+  //===============
+  // Input options
+  //===============
+  // verbosity setting (applies to all input managers)
+  Input::VERBOSITY = 1; // so we get prinouts of the event number
+  Input::PYTHIA8 = true;
+  PYTHIA8::config_file = string(getenv("CALIBRATIONROOT")) + "/Generators/HeavyFlavor_TG/phpythia8_minBias_MDC2.cfg";
+  
+  //INPUTHEPMC::filename = inputFile;
+  //INPUTHEPMC::FLOW = true;
+  //INPUTHEPMC::FLOW_VERBOSITY = 3;
+  //INPUTHEPMC::FERMIMOTION = true;
+
+  // Event pile up simulation with collision rate in Hz MB collisions.
+  //Input::PILEUPRATE = 100e3;
+
+  //-----------------
+  // Initialize the selected Input/Event generation
+  //-----------------
+  // This creates the input generator(s)
+  InputInit();
+
+  //--------------
+  // Set Input Manager specific options
+  //--------------
+  // can only be set after InputInit() is called
+
+  if (Input::PYTHIA8)
+  {
+    Input::ApplysPHENIXBeamParameter(INPUTGENERATOR::Pythia8);
+  }
+  if (Input::HEPMC)
+  {
+    //! apply sPHENIX nominal beam parameter with 2mrad crossing as defined in sPH-TRG-2020-001
+    Input::ApplysPHENIXBeamParameter(INPUTMANAGER::HepMCInputManager);
+    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_width(0.02, 0.02, 10., 0);  //collision vertex from CAD (sigma = 200um in x/y, 7.5cm in z)
+                                                                                           //    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_mean(0,0,0,0);//optional collision central position shift in space, time
+    // //optional choice of vertex distribution function in space, time
+    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_function(PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus);
+    //! embedding ID for the event
+    //! positive ID is the embedded event of interest, e.g. jetty event from pythia
+    //! negative IDs are backgrounds, .e.g out of time pile up collisions
+    //! Usually, ID = 0 means the primary Au+Au collision background
+    //INPUTMANAGER::HepMCInputManager->set_embedding_id(Input::EmbedID);
+    if (Input::PILEUPRATE > 0)
+    {
+      // Copy vertex settings from foreground hepmc input
+      INPUTMANAGER::HepMCPileupInputManager->CopyHelperSettings(INPUTMANAGER::HepMCInputManager);
+      // and then modify the ones you want to be different
+      // INPUTMANAGER::HepMCPileupInputManager->set_vertex_distribution_width(100e-4,100e-4,8,0);
+    }
+  }
+  // register all input generators with Fun4All
+  InputRegister();
+
+  SyncReco *sync = new SyncReco();
+  se->registerSubsystem(sync);
+
+  HeadReco *head = new HeadReco();
+  se->registerSubsystem(head);
+
+  FlagHandler *flag = new FlagHandler();
+  se->registerSubsystem(flag);
+
+  // set up production relatedstuff
+    Enable::PRODUCTION = true;
+
+  //======================
+  // Write the DST
+  //======================
+
+  Enable::DSTOUT = true;
+
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  //======================
+  // What to run
+  //======================
+  // Global options (enabled for all enables subsystems - if implemented)
+  //  Enable::ABSORBER = true;
+  //  Enable::OVERLAPCHECK = true;
+  //  Enable::VERBOSITY = 1;
+
+  Enable::BBC = true;
+
+  Enable::PIPE = true;
+
+  // central tracking
+  Enable::MVTX = true;
+
+  Enable::INTT = true;
+
+  Enable::TPC = true;
+
+  Enable::MICROMEGAS = true;
+
+  Enable::CEMC = true;
+
+  Enable::HCALIN = true;
+  G4HCALIN::light_scint_model = 20;
+
+  Enable::MAGNET = true;
+
+  Enable::HCALOUT = true;
+  G4HCALOUT::light_scint_model = 20;
+
+  Enable::EPD = true;
+
+  //! forward flux return plug door. Out of acceptance and off by default.
+  Enable::PLUGDOOR = true;
+  //Enable::PLUGDOOR_BLACKHOLE = true;
+
+  //Enable::BEAMLINE = true;
+  G4BEAMLINE::skin_thickness = 0.5;
+
+  Enable::ZDC = true;
+
+  // new settings using Enable namespace in GlobalVariables.C
+  Enable::BLACKHOLE = true;
+  Enable::BLACKHOLE_FORWARD_SAVEHITS = false; // disable forward/backward hits
+  //Enable::BLACKHOLE_SAVEHITS = false; // turn off saving of bh hits
+  //BlackHoleGeometry::visible = true;
+
+  //---------------
+  // World Settings
+  //---------------
+  // G4WORLD::PhysicsList = "FTFP_BERT_HP"; //FTFP_BERT_HP best for calo
+  //  G4WORLD::WorldMaterial = "G4_AIR"; // set to G4_GALACTIC for material scans
+
+  //---------------
+  // Magnet Settings
+  //---------------
+
+  //  const string magfield = "1.5"; // alternatively to specify a constant magnetic field, give a float number, which will be translated to solenoidal field in T, if string use as fieldmap name (including path)
+  //  G4MAGNET::magfield = string(getenv("CALIBRATIONROOT")) + string("/Field/Map/sPHENIX.2d.root");  // default map from the calibration database
+  // G4MAGNET::magfield_rescale = -1.4 / 1.5;  // make consistent with expected Babar field strength of 1.4T
+
+
+  // Initialize the selected subsystems
+  G4Init();
+
+  //---------------------
+  // GEANT4 Detector description
+  //---------------------
+  if (!Input::READHITS)
+  {
+    G4Setup();
+  }
+
+  string outputroot = outputFile;
+  string remove_this = ".root";
+  size_t pos = outputroot.find(remove_this);
+  if (pos != string::npos)
+  {
+    outputroot.erase(pos, remove_this.length());
+  }
+
+  //--------------
+  // Set up Input Managers
+  //--------------
+
+  InputManagers();
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+
+  if (Enable::DSTOUT)
+  {
+    string FullOutFile = DstOut::OutputFile;
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
+    if (Enable::DSTOUT_COMPRESS) DstCompress(out);
+    se->registerOutputManager(out);
+  }
+  //-----------------
+  // Event processing
+  //-----------------
+
+  // if we use a negative number of events we go back to the command line here
+  if (nEvents < 0)
+  {
+    return 0;
+  }
+  // if we run the particle generator and use 0 it'll run forever
+  if (nEvents == 0 && !Input::HEPMC && !Input::READHITS)
+  {
+    cout << "using 0 for number of events is a bad idea when using particle generators" << endl;
+    cout << "it will run forever, so I just return without running anything" << endl;
+    return 0;
+  }
+
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+
+  XploadInterface::instance()->Print(); // print used DB files
+  se->End();
+  se->PrintTimer();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+
+  gSystem->Exit(0);
+  return 0;
+}
+#endif
